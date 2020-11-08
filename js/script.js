7bef66e467880a2276289910c6d024b2ad939533
Olha <nadolechka@gmail.com>
2020-11-08 12:33:37 +0200

    commit 3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 "use strict";
 
-// const category = 'toys';
-// console.log(`https://test.com/${category}/5`);
+console.log(4 + +'5');
 
-const user = "Olha";
-alert(`hi, ${user}`);
+let incr = 10,
+decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== 6);
+
+const isChecked = false,
+isClose = false;
+
+console.log(isChecked || !isClose);
+
